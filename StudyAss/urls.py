1ec09f317400d6b0710fe951daf9b9bde33c53ef
senseikh <hillarykisera@gmail.com>
2023-02-08 11:02:54 +0300

    sEC

--- StudyAss/urls.py
+++ StudyAss/urls.py
@@ -14,15 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-
-def home(request):
-    return HttpResponse ('HomePage')
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include ('base.urls'))
 ]
